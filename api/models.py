c612f73991d43278a33b3de7357da1ae6502c930
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-07-03 13:49:13 +0200

    tasks api

--- api/models.py
+++ api/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("low", "Low"),
+        ("middle", "Middle"),
+        ("high", "High"),
+    ]
+
+    task = models.CharField(max_length=127, unique=True)
+    description = models.TextField(blank=True, null=True, max_length=255)
+    priority = models.CharField(
+        max_length=7, choices=PRIORITY_CHOICES, default="middle"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task
