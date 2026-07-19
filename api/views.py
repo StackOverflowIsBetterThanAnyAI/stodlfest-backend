266afebd0d7cd070b64e2f0fb5adaa64c81e1d28
StackOverflowIsBetterThanAnyAI <michael.muenzenhofer@gmail.com>
2026-07-19 17:57:17 +0200

    remove underage members from job if job turns out to require users of legal age

--- api/views.py
+++ api/views.py
@@ -7,6 +7,12 @@ class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all().order_by("job")
     serializer_class = JobSerializer
 
+    def perform_update(self, serializer):
+        instance = serializer.save()
+
+        if instance.requires_legal_age == "doesRequireLegalAge":
+            instance.assigned_members.filter(age="underage").update(job=None)
+
 
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all().order_by("surname")
